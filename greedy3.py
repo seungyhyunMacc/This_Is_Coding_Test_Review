cf6352b5a6b5cfddc0efdc6a37c42ff340261412
seunghyunMacc <harry2430@naver.com>
2022-01-13 23:17:33 +0900

    modified-이것이 코딩테스트다-그리디3

--- greedy3.py
+++ greedy3.py
@@ -1,17 +1,10 @@
 n,m=map(int, input('행과 열의 크기를 설정하세요: ').split())
-# data=[]
+data=[]
 answer=0
-# compared=[]
-# for i in range(n):
-#     data.append(list(map(int, input('행을 순서대로 입력해주세요: ').split())))
-#     compared.append(min(data[i]))
-#     answer=max(compared)
-
-# print(answer)
-
+compared=[]
 for i in range(n):
-    data=list(map(int, input('행을 순서대로 입력해주세요: ').split()))
-    compared=min(data)
-    answer = max(answer, compared)
+    data.append(list(map(int, input('행을 순서대로 입력해주세요: ').split())))
+    compared.append(min(data[i]))
+    answer=max(compared)
 
 print(answer)
